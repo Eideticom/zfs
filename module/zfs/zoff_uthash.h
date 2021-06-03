910781c75490a4b313cc357d2b7d854cce34cc5f
Jason Lee <jasonlee@lanl.gov>
2021-06-03 13:25:25 -0600

    uthash overrides only needed in kernel

--- module/zfs/zoff_uthash.h
+++ module/zfs/zoff_uthash.h
@@ -3,9 +3,12 @@
 
 #include <sys/zfs_context.h>
 
+#ifdef _KERNEL
 #define	uthash_malloc(sz)   kmem_alloc(sz, KM_SLEEP)
 #define	uthash_free(ptr, sz) kmem_free(ptr, sz)
 #define	uthash_fatal(msg)
+#endif
+
 #include "uthash.h"
 
 #endif
